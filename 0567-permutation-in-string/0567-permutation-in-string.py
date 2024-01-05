b3d949d41dc32f649f67025bc1839424d3231c63
Kai Ma <77073162+k78ma@users.noreply.github.com>
2024-01-04 18:42:55 -0700

    Time: 44 ms (99.52%), Space: 17.4 MB (13.88%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        if len(s1) > len(s2):
+            return False
+
+        s1_count, window_count = {}, {}
+        
+        for ch in s1:
+            s1_count[ch] = s1_count.get(ch, 0) + 1
+
+        for i in range(len(s2)):
+            # Add new character to the window
+            window_count[s2[i]] = window_count.get(s2[i], 0) + 1
+
+            if i >= len(s1):
+                if window_count[s2[i - len(s1)]] == 1:
+                    del window_count[s2[i - len(s1)]]
+                else:
+                    window_count[s2[i - len(s1)]] -= 1
+
+            if window_count == s1_count:
+                return True
+
+        return False
