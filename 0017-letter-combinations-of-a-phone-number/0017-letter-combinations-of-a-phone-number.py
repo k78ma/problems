07a58b59a2c2590380d3e3bf3a6d60e931b7dfb8
Kai Ma <77073162+k78ma@users.noreply.github.com>
2024-02-28 16:30:17 -0800

    Time: 35 ms (61.48%), Space: 16.5 MB (61.98%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -14,12 +14,12 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         
         def dfs(start_index, path):
-            if len(path) == len(digits):
+            if start_index == len(digits):
                 result.append(''.join(path))
                 return
             
-            next_num = digits[start_index]
-            for letter in self.KEYBOARD[next_num]:
+            current_num = digits[start_index]
+            for letter in self.KEYBOARD[current_num]:
                 path.append(letter)
                 dfs(start_index + 1, path)
                 path.pop()
