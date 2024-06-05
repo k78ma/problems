525bc463d74c474f48755913be725756ea895242
Kai Ma <77073162+k78ma@users.noreply.github.com>
2024-06-04 22:40:45 -0400

    Time: 478 ms (57.40%), Space: 11.7 MB (64.33%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -7,23 +7,27 @@ class Solution(object):
         """
         
         nums.sort()
-        n = len(nums)
-        closest_sum = nums[0] + nums[1] + nums[2] 
-
-        for i in range(n - 2):
-            left, right = i + 1, n - 1  # 2ptrs
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
+        closest_sum = nums[0] + nums[1] + nums[2]
+        
+        for idx_1 in range(len(nums) - 2):
+            
+            l, r  = idx_1 + 1, len(nums) - 1
+            
+            while l < r:
+                current_sum = nums[idx_1] + nums[l] + nums[r]
+                diff = current_sum - target
                 
-                # if the current sum is closer to the target, update closest_sum
-                if abs(current_sum - target) < abs(closest_sum - target):
+                if abs(diff) < abs(closest_sum - target):
                     closest_sum = current_sum
-
+                
                 if current_sum < target:
-                    left += 1 
+                    l += 1
                 elif current_sum > target:
-                    right -= 1
+                    r -= 1
                 else:
-                    return closest_sum  # Exact match
-
+                    return closest_sum
+            
         return closest_sum
+            
+            
+            
