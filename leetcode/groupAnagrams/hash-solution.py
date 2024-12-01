138ad3c67f8309509ffefc9d82885d48345ab250
Kai Ma <k78ma@uwaterloo.ca>
2024-12-01 01:46:26 -0500

    bleh2

---
+++ leetcode/groupAnagrams/hash-solution.py
@@ -0,0 +1,17 @@
+from typing import List
+from collections import defaultdict
+
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    
+    result = defaultdict(list)
+
+    for s in strs:
+        count = [0] * 26
+
+        for c in s:
+            count[ord(c) - ord("a")] += 1
+
+        result[tuple(count)].append(s)
+
+    return result.values()
+
